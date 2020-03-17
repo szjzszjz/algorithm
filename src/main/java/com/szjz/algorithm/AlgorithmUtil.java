6c5eabcde9aff8f14eda2e4d31223df2466a2478
史博 <312685889@qq.com>
2020-03-17 18:17:37 +0800

    add

--- src/main/java/com/szjz/algorithm/AlgorithmUtil.java
+++ src/main/java/com/szjz/algorithm/AlgorithmUtil.java
@@ -1,5 +1,7 @@
 package com.szjz.algorithm;
 
+import java.util.Stack;
+
 /**
  * author:szjz
  * date:2019/7/17
@@ -9,20 +11,23 @@ public class AlgorithmUtil {
 
     public static void main(String[] args) {
 
-        String str = "cabcdc";
-        int length = lengthOfLongestSubstring(str);
-        System.err.println(length);
+//        String str = "cabcdc";
+//        int length = lengthOfLongestSubstring(str);
+//        System.err.println(length);
+
+        System.out.println(isValid("{}"));
+
     }
 
 
     /**
      * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
      * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-     *
+     * <p>
      * 输入: "pwwkew"
      * 输出: 3
      * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-     *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
      */
     public static int lengthOfLongestSubstring(String s) {
         int l = 0;   //记录子串中第一次出现重复字符的最左边的位置
@@ -36,5 +41,32 @@ public class AlgorithmUtil {
         }
         return dp[s.length()];
     }
+
+    public  static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                Character popC = stack.pop();
+                if (c == ')' && popC != '(') {
+                    return false;
+                }
+                if (c == '}' && popC != '{') {
+                    return false;
+                }
+                if (c == ']' && popC != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
 
 }
